e7968510e6fd6808aa42508d07a358afe0e2d2d0
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-11-05 02:41:29 -0500

    Intial Commit. Created a Starter Spring project

---
+++ src/main/java/com/runtimeterror/milkyways/MilkywaysApplication.java
@@ -0,0 +1,13 @@
+package com.runtimeterror.milkyways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkywaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkywaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runtimeterror/milkyways/MilkywaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtimeterror.milkyways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkywaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
