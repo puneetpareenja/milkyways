eab15d93ce40ef9b95b6689855b90c6f9b0de7bc
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-11-08 14:27:15 -0800

    Merge branch 'master' of C:\Devil\Code\IdeaProjects\milkyways with conflicts.

---
+++ src/main/java/com/runtimeterror/milkyways/entities/Inventory.java
@@ -0,0 +1,9 @@
+package com.runtimeterror.milkyways.entities;
+
+public class Inventory {
+//    private int itemid;
+//    private int bakeryid;
+//    private int quantity;
+
+
+}
--- src/main/java/com/runtimeterror/milkyways/entities/MenuItem.java
+++ src/main/java/com/runtimeterror/milkyways/entities/MenuItem.java
@@ -1,6 +1,14 @@
 package com.runtimeterror.milkyways.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int itemid;
     private int bakeryid;
     private String name;
