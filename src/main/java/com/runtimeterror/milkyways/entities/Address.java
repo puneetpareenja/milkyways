d6ecae291224af0c485a31222a55591544cacd81
navpreet kaur <kaurn45@student.douglascollege.ca>
2019-11-20 13:41:32 -0800

    updated address entity.

--- src/main/java/com/runtimeterror/milkyways/entities/Address.java
+++ src/main/java/com/runtimeterror/milkyways/entities/Address.java
@@ -15,4 +15,55 @@ public class Address {
     private String city;
     private String postalcode;
 
+    public Address() {
+
+    }
+
+    public Address(String apt, String street, String city, String postalcode) {
+        this.apt = apt;
+        this.street = street;
+        this.city = city;
+        this.postalcode = postalcode;
+    }
+
+    public long getAddressid() {
+        return addressid;
+    }
+
+    public void setAddressid(long addressid) {
+        this.addressid = addressid;
+    }
+
+    public String getApt() {
+        return apt;
+    }
+
+    public void setApt(String apt) {
+        this.apt = apt;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
 }
