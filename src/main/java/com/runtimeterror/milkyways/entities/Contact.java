0a1ee8080a73ec088a252964afc02fabc10ce02c
chhavi-kapur <kapurc1@student.douglascollege.ca>
2019-11-21 23:26:34 -0800

    Added entity-Contact to store messages sent by customers

---
+++ src/main/java/com/runtimeterror/milkyways/entities/Contact.java
@@ -0,0 +1,9 @@
+package com.runtimeterror.milkyways.entities;
+
+public class Contact {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String subject;
+    private String message;
+}
