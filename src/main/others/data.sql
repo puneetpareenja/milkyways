77f8bd2d1de1635eb2236d904181c4d1973bed7d
chhavi-kapur <kapurc1@student.douglascollege.ca>
2019-11-25 17:26:08 -0800

    Added item and store data to H2

--- src/main/others/data.sql
+++ src/main/others/data.sql
@@ -1,6 +1,6 @@
-insert into item (itemid, name, imageurl, quantity, type, sugarfree, glutenfree, dairyfree, vegetarian, price, rating)
-values (1, 'Birthday letter donuts', 'birthday-donuts.jpg', 9, true, false, false , false, false , 90.00, 5);
-
-
 -- Store insert Command
--- INSERT INTO STORE ValUES (1, '1234567890', 'Purebread');
+-- INSERT INTO STORE VAlUES (1, '1234567890', 'Purebread');
+
+
+--insert into item (itemid, name, description, imageurl, quantity, type, sugarfree, glutenfree, dairyfree, vegetarian, price, rating, store_storeid)
+--values (1, 'Birthday letter donuts', 'For your loved ones, an untraditional alternative to cake. 13 letter donuts spelling out H.A.P.P.Y B.I.R.T.H.D.A.Y. Available in flavors of your choice including chocolate, vanilla bean, birthday cake and maple.', 'birthday-donuts.jpg', 9, true, false, false , false, false , 90.00, 5, 2);
