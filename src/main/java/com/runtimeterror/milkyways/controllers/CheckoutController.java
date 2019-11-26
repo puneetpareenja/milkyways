6bceb0c30502443a551c54da935db4e5b5def8c4
chhavi-kapur <kapurc1@student.douglascollege.ca>
2019-11-25 22:45:30 -0800

    Creating cart

--- src/main/java/com/runtimeterror/milkyways/controllers/CheckoutController.java
+++ src/main/java/com/runtimeterror/milkyways/controllers/CheckoutController.java
@@ -1,8 +1,17 @@
 package com.runtimeterror.milkyways.controllers;
 
+import com.runtimeterror.milkyways.entities.Customer;
+import com.runtimeterror.milkyways.entities.Item;
+import com.runtimeterror.milkyways.repositories.CustomerRepository;
+import com.runtimeterror.milkyways.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 public class CheckoutController {
@@ -13,10 +22,35 @@ public class CheckoutController {
         return modelAndView;
     }
 
+    /*
     @GetMapping("/cart")
     public ModelAndView redirectToCart() {
         ModelAndView modelAndView = new ModelAndView("cart.html");
 
+        return modelAndView;
+    } */
+
+
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    @GetMapping("/cart")
+    public ModelAndView redirectToCart(HttpSession session, @RequestParam(defaultValue = "0") long id, @RequestParam(defaultValue= "1") int quantity) {
+        ModelAndView modelAndView = new ModelAndView("cart.html");
+        Item item = itemRepository.findById(id).orElse(new Item());
+        //get customer from session
+        Customer customer = (Customer) session.getAttribute("customer");
+        if (customer == null) {
+            customer = new Customer();
+        }
+        //create the list
+        item.setQuantity(quantity);
+        customer.getCart().add(item);
+
+        modelAndView.addObject(item);
         return modelAndView;
     }
 }
