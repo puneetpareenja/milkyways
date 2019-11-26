1f98254f63fb1e647573522af119655cbeacede9
navpreet kaur <kaurn45@student.douglascollege.ca>
2019-11-26 00:11:23 -0800

    reverted the corrupted database in the last commit.

--- src/main/java/com/runtimeterror/milkyways/repositories/ItemRepository.java
+++ src/main/java/com/runtimeterror/milkyways/repositories/ItemRepository.java
@@ -1,9 +1,20 @@
 package com.runtimeterror.milkyways.repositories;
 
 import com.runtimeterror.milkyways.entities.Item;
+
 import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
+
     public List<Item> findAllByNameContainingIgnoreCase(String name);
+
+    public List<Item> findAllByGlutenfreeIs(boolean type);
+
+    public List<Item> findAllBySugarfreeIs(boolean type);
+
+    public List<Item> findAllByDairyfreeIs(boolean type);
+
+    public List<Item> findAllByVegetarianIs(boolean type);
 }
