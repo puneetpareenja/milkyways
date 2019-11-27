484ec4f4d2e525a65e1fe12fc3c8ed881051adbf
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-11-26 22:28:07 -0800

    Update checkout.html to display infromation after login. Created a SalesRepository to save sales information. Update /thankyou requestmapping to save the sales information and reset the cart.

--- src/main/java/com/runtimeterror/milkyways/controllers/RoutingController.java
+++ src/main/java/com/runtimeterror/milkyways/controllers/RoutingController.java
@@ -1,15 +1,26 @@
 package com.runtimeterror.milkyways.controllers;
 
 import com.runtimeterror.milkyways.entities.Contact;
+import com.runtimeterror.milkyways.entities.Customer;
+import com.runtimeterror.milkyways.entities.Item;
+import com.runtimeterror.milkyways.entities.Sales;
 import com.runtimeterror.milkyways.repositories.ContactRepository;
+import com.runtimeterror.milkyways.repositories.SalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Date;
+
 @Controller
 public class RoutingController {
+
+    @Autowired
+    SalesRepository salesRepository;
 
     @GetMapping("/about")
     public ModelAndView redirectToAbout() {
@@ -19,9 +30,21 @@ public class RoutingController {
 
 
     @GetMapping("/thankyou")
-    public ModelAndView redirectToThankyou() {
+    public ModelAndView redirectToThankyou(HttpSession session) {
+        Customer customer = (Customer) session.getAttribute("customer");
+        Sales sales = new Sales();
+        sales.setCustomer(customer);
+        sales.setDate(new Date());
+        sales.setItemlist(customer.getCart());
+        double total = 0;
+        for (Item item : customer.getCart()) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        sales.setTotal(total);
+        salesRepository.save(sales);
+        customer.setCart(new ArrayList<>());
+        session.setAttribute("customer", customer);
         ModelAndView modelAndView = new ModelAndView("thankyou.html");
-
         return modelAndView;
     }
 
---
+++ src/main/java/com/runtimeterror/milkyways/repositories/SalesRepository.java
@@ -0,0 +1,7 @@
+package com.runtimeterror.milkyways.repositories;
+
+import com.runtimeterror.milkyways.entities.Sales;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalesRepository extends CrudRepository<Sales, Long> {
+}
